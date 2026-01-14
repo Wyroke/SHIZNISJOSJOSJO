0752958138fad1e17d5e974e5397cdf54fa87fec
Wyroke <wavtheprime@gmail.com>
2026-01-13 20:22:47 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,7 +10,7 @@ if ARGS.Refresh then
     if not isfolder('ReVape') then
         print('Successfully deleted the "ReVape" folder!')
     else
-        warn('Had an issue deleting the "ReVape" folder. Please DM the user "ye40" on Discord!')
+        warn('Had an issue deleting the "ReVape" folder. Please DM the user "7ykt" on Discord!')
     end
 end
 
@@ -42,7 +42,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/Wyroke/SHIZNISJOSJOSJO/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -92,7 +92,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/soryed/ReVapeForRoblox') 
+		return game:HttpGet('https://github.com/Wyroke/SHIZNISJOSJOSJO') 
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
